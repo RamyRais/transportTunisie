f7008d43dba652814e4b7ab400ede0c54f7ad705
ramy rais <ramy.rais@gmail.com>
2016-02-04 20:10:31 +0100

    remove some stuff for copy paste

--- server/router.js
+++ server/router.js
@@ -25,14 +25,7 @@ if (env === 'development') {
 }
 //TODO change to src to dist in production done just for test but must be removed after setting the scripts
 if (env === 'production') {
-  server.use(express.static(path.resolve(__dirname + '/../public/src'), {
-    maxAge: '1y',
-    setHeaders: function(res, path) {
-      if (mime.lookup(path) === 'text/html') {
-        res.setHeader('Cache-Control', 'public, max-age=0')
-      }
-    }
-  }));
+  server.use(express.static(path.resolve(__dirname + '/../public/src')));
   server.get('/', function(req, res, next) {
     res.sendFile(path.resolve(__dirname + '/../public/src/index.html'), {
       maxAge: 0
